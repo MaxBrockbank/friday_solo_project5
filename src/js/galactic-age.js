11c11f804041902e2b0118e1ef42380ea646f25c
Max Brockbank <maxbrockbank1999@gmail.com>
2020-11-13 09:21:05 -0800

    good fail of spec 2 test

--- src/js/galactic-age.js
+++ src/js/galactic-age.js
@@ -3,7 +3,7 @@ export default class Galactic_Age {
 
   constructor(name, age){
     this.name = name,
-    this.earthAge = parseInt(age),
+    this.earthAge = parseInt(age)
     // this.mercurysAge = Math.round((365/88) * this.earthAge)
 
   }
